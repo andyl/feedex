6bf8dd07aa160467654a89c201c83e309bc9f1d6
andyl <andy@r210.com>
2019-07-31 17:26:57 -0700

    Update runner

--- apps/ragged_runner/lib/ragged_runner.ex
+++ apps/ragged_runner/lib/ragged_runner.ex
@@ -1,18 +1,35 @@
 defmodule RaggedRunner do
   @moduledoc """
-  Documentation for RaggedRunner.
+  Background job scheduler / runner.
+
+  Parameters:
+  - state: on/off (default off)
+  - cycle_frequency - seconds between cycles (default 300)
+  - max_concurrent_jobs - default 5
+  - min_update_frequency - default every 30 minutes
+
+  Config params specified at startup.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RaggedRunner.hello()
-      :world
-
+  Start the RaggedRunner.
   """
-  def hello do
-    :world
+  def start do
   end
+
+  @doc """
+  Stop the RaggedRunner.
+  """
+  def stop do
+  end
+
+  def config_show do
+  end
+
+  def config_update do
+  end
+
+  # defp select_jobs do
+  #   RaggedData.Ctx.News.stale_feeds(config)
+  # end
 end

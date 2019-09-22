c5a428d041541fa65dff071b1d93400743e6e210
andyl <andy@r210.com>
2019-09-21 21:04:39 -0700

    Update javascript

--- apps/ragged_web/assets/js/app.js
+++ apps/ragged_web/assets/js/app.js
@@ -4,8 +4,9 @@ import css from "../css/app.scss"
 
 import "phoenix_html"
 
+import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live")
+let liveSocket = new LiveSocket("/live", Socket, {...})
 liveSocket.connect()
 

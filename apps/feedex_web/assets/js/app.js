2c589a73d7ccb009ff955721d13c44588662c1d3
andyl <andy@r210.com>
2019-10-12 11:46:18 -0700

    Cleanup

--- apps/feedex_web/assets/js/app.js
+++ apps/feedex_web/assets/js/app.js
@@ -1,8 +1,10 @@
 
-import "bootstrap";
+import "bootstrap"
 import css from "../css/app.scss"
 
 import "phoenix_html"
+
+// assets/js/app.js
 
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"

fa3cd602b32b923a910add541092ae40eb23a4a9
andyl <andy@r210.com>
2020-07-07 09:09:58 -0700

    Update badger image

--- apps/feedex_web/lib/feedex_web/views/layout_view.ex
+++ apps/feedex_web/lib/feedex_web/views/layout_view.ex
@@ -39,7 +39,7 @@ defmodule FeedexWeb.LayoutView do
   def footer_img(conn) do
     r_path = conn.request_path
     """
-    <img src="https://badger.casmacc.net/pngF/1?path=#{r_path}">
+    <img src="https://badger.casmacc.net/pngF/081484?path=#{r_path}">
     """ |> Phoenix.HTML.raw()
   end
 end

eb5aa9e3fde082be94af2457edd4e7d9fda75f11
andyl <andy@r210.com>
2023-03-12 13:22:48 -0700

    Update dotfiles

---
+++ .iex.exs
@@ -0,0 +1,21 @@
+IO.puts("--------------------------- FEEDEX DATA ---------------------------")
+
+import_file_if_available("~/.iex.exs")
+
+alias FeedexData.Ctx.Account
+alias FeedexData.Ctx.Account.{User, Folder, Register, ReadLog}
+
+alias FeedexData.Ctx.News
+alias FeedexData.Ctx.News.{Feed, Post}
+
+alias FeedexData.Api
+alias FeedexData.Api.Subs
+
+alias FeedexUi.Cache.UiState
+
+
+alias FeedexData.Repo
+alias FeedexData.Repo, as: R
+
+import_if_available(Ecto.Query)
+

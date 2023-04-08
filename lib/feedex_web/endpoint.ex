74151c4a2f0d7310dd170c7a6dc919d6b1323d33
andyl <andy@r210.com>
2023-04-08 10:08:28 -0700

    Add service

--- lib/feedex_web/endpoint.ex
+++ lib/feedex_web/endpoint.ex
@@ -11,6 +11,8 @@ defmodule FeedexWeb.Endpoint do
     same_site: "Lax"
   ]
 
+  IO.puts("ENDPOINT_START")
+
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.

fc3ea220958bd40072937d9c2c3fa809ff9f68f6
andyl <andy@r210.com>
2024-07-04 11:41:52 -0700

    Update build task

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -4,11 +4,8 @@ defmodule Mix.Tasks.Build do
   @shortdoc "Builds the project and records the timestamp of the build"
 
   def run(_args) do
-    # Record the current timestamp
-    timestamp = DateTime.utc_now() |> Calendar.strftime("%y-%m-%d_%H:%M UTC")
-
-    # Write the timestamp to a file
-    File.write!("built_at.txt", timestamp)
+    # Write build info to file
+    Util.BuildInfo.write()
 
     # Run the standard mix compile task
     Mix.Task.run("compile")

57abf3f73abf3beef12c02642c4a7ccf3e0c8601
andyl <andy@r210.com>
2024-07-04 13:07:20 -0700

    Add inspect

--- lib/feedex_web/controllers/page_controller.ex
+++ lib/feedex_web/controllers/page_controller.ex
@@ -4,6 +4,8 @@ defmodule FeedexWeb.PageController do
   def home(conn, _params) do
     values = Util.BuildInfo.read()
 
+    IO.inspect(values, label: "TANGOFOR")
+
     render(conn, :home, layout: false, attributes: values)
   end
 end

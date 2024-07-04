d35827615b1db98f623c53ce562729873eebd356
andyl <andy@r210.com>
2024-07-04 12:48:15 -0700

    Fix assignment

--- lib/feedex_web/controllers/page_controller.ex
+++ lib/feedex_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule FeedexWeb.PageController do
   use FeedexWeb, :controller
 
   def home(conn, _params) do
-    {:ok, values} = Util.BuildInfo.read()
+    values = Util.BuildInfo.read()
 
     render(conn, :home, layout: false, attributes: values)
   end

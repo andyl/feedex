46190ea9ad38f6390f87174c0a7fc0b8084ad7b8
andyl <andy@r210.com>
2024-07-21 18:53:12 -0700

    Add LiveReload handlers

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,6 +30,26 @@ topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.delayedShow(200))
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
+window.addEventListener("phx:live_reload:attached", ({detail: reloader}) => {
+  // Enable server log streaming to client. Disable with reloader.disableServerLogs()
+  //   * click with "d" key pressed to open at function component definition location
+  let keyDown
+  window.addEventListener("keydown", e => keyDown = e.key)
+  window.addEventListener("keyup", e => keyDown = null)
+  window.addEventListener("click", e => {
+    if(keyDown === "c"){
+      e.preventDefault()
+      e.stopImmediatePropagation()
+      reloader.openEditorAtCaller(e.target)
+    } else if(keyDown === "d"){
+      e.preventDefault()
+      e.stopImmediatePropagation()
+      reloader.openEditorAtDef(e.target)
+    }
+  }, true)
+  window.liveReloader = reloader
+})
+
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 

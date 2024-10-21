c2ed27f1d390bbbe22eb52679c4d97de1085326a
andyl <andy@r210.com>
2024-10-21 09:29:48 -0700

    fix: prevent seed loading when mix.env == test

--- lib/feedex/seeds.ex
+++ lib/feedex/seeds.ex
@@ -6,30 +6,36 @@ defmodule Feedex.Seeds do
   import Ecto.Query
 
   def load_if_empty do
-    require Logger
-    Logger.info("------------------------------------------------------")
-    Logger.info("----- SEED_LOAD_IF_EMPTY")
-    Logger.info("------------------------------------------------------")
-    qry = from u in "users", select: u.id
-    num = Repo.all(qry) |> length
+    if Mix.env() != :test do
+      require Logger
+      Logger.info("------------------------------------------------------")
+      Logger.info("----- SEED_LOAD_IF_EMPTY")
+      Logger.info("------------------------------------------------------")
+      qry = from u in "users", select: u.id
+      num = Repo.all(qry) |> length
 
-    if num == 0 do
-      Logger.info("----- No users - loading seeds...")
-      load()
-    else
-      Logger.info("----- Users exist - skipping seeds...")
+      if num == 0 do
+        Logger.info("----- No users - loading seeds...")
+        load()
+      else
+        Logger.info("----- Users exist - skipping seeds...")
+      end
+
+      Logger.info("------------------------------------------------------")
     end
-
-    Logger.info("------------------------------------------------------")
   end
 
-  def load do
+  def clear_all do
     Repo.delete_all(ReadLog)
     Repo.delete_all(Register)
     Repo.delete_all(Folder)
     Repo.delete_all(Post)
     Repo.delete_all(Feed)
     Repo.delete_all(User)
+  end
+
+  def load do
+    clear_all()
 
     Repo.insert(%User{
       name: "aaa",
